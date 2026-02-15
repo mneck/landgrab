4bc1b60b098ab8b10c937ad955028428f345a18d
matt <mneckelmann@gmail.com>
2026-02-15 01:57:44 +0100

    Add Start screen

---
+++ landgrab/scripts/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+## Main menu scene for Landgrab.
+## Displays a Start button to begin a game.
+
+
+func _on_start_button_pressed() -> void:
+	# TODO: Transition to game scene when it exists
+	print("Game started!")
